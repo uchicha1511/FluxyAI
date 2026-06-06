fdee73f83f4b462865adc5e773a5ca771225d197
few-4 <bnnahariya@gmail.com>
2026-06-06 14:18:10 +0530

    feat: implement Mistral AI integration with streaming responses and chat title generation

--- FluxyAI-server/src/repositories/contracts/IAIRepository.js
+++ FluxyAI-server/src/repositories/contracts/IAIRepository.js
@@ -2,6 +2,9 @@ class IAIRepository {
   async streamResponse(message, onChunk) {
     throw new Error("Method not implemented");
   }
+  async generateTitle(message, onChunk) {
+    throw new Error("Method not implemented");
+  }
 }
 
 export default IAIRepository;
