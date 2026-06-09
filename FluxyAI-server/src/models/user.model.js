298e6fbe62435416e5d4a5f37bb8da12784c2c4e
few-4 <bnnahariya@gmail.com>
2026-06-10 02:22:47 +0530

    fix: fix User model with password hashing and verification methods

--- FluxyAI-server/src/models/user.model.js
+++ FluxyAI-server/src/models/user.model.js
@@ -32,7 +32,7 @@ userSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
     
 
-  this.password = await bcrypt.hash(this.password, 10);
+  this.password = bcrypt.hash(this.password, 10);
  
 });
 
